4de6580d8615e49e6072ece90e20778f5e007c2b
Maurice Johnson <maurice.johnson9911@gmail.com>
2021-10-22 13:30:19 -0400

    init commit

---
+++ src/main/java/com/asset/tracker/nft/NftApplication.java
@@ -0,0 +1,13 @@
+package com.asset.tracker.nft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftApplication.class, args);
+	}
+
+}
