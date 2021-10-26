787f3803b606fdcef90ebc46d6af593289182f6a
Maurice Johnson <maurice.johnson9911@gmail.com>
2021-10-26 16:26:02 -0400

    adding models

--- src/main/java/com/asset/tracker/nft/model/NFTCollection.java
+++ src/main/java/com/asset/tracker/nft/model/NFTCollection.java
@@ -1,15 +1,62 @@
 package com.asset.tracker.nft.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "nft_collection")
 public class NFTCollection {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "eth_address")
     private String ethAddress;
+
+    @Column(name="floor_price")
     private double floorPrice;
 
-    public NFTCollection(String name, String ethAddress, double floorPrice) {
+    @Column(name="quantity")
+    private long quantity;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "portfolios",
+            joinColumns = @JoinColumn(
+                    name = "collection_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "portfolio_id", referencedColumnName = "id"))
+    private List<Portfolio> portfolios;
+
+    public NFTCollection() {
+    }
+
+    public NFTCollection(String name, String ethAddress, double floorPrice, long quantity) {
         this.name = name;
         this.ethAddress = ethAddress;
         this.floorPrice = floorPrice;
+        this.quantity = quantity;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -34,5 +81,13 @@ public class NFTCollection {
 
     public void setFloorPrice(double floorPrice) {
         this.floorPrice = floorPrice;
+    }
+
+    public List<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(List<Portfolio> portfolios) {
+        this.portfolios = portfolios;
     }
 }
---
+++ src/main/java/com/asset/tracker/nft/model/User.java
@@ -0,0 +1,123 @@
+package com.asset.tracker.nft.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "portfolio_id", referencedColumnName = "id")
+    private Portfolio portfolio;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String username, String email,
+                String password, Portfolio portfolio, Collection<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.portfolio = portfolio;
+        this.roles = roles;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+}
